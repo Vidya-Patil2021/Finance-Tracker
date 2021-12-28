79fdd0ed961e0ab2515b3cb879af2ed3773fc13d
Vidya-Patil2021 <Vidya@excelleresolutions.com>
2021-12-28 18:01:05 +0530

    Setup and use API key to get stock data

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -56,4 +56,5 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+  os.environ['IEX_API_VERSION'] = 'iexcloud-sandbox'
 end

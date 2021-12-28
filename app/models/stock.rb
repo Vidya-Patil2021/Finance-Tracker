00635250dd2c049975c855be0f6f154c8cf7c71e
Vidya-Patil2021 <Vidya@excelleresolutions.com>
2021-12-28 20:44:32 +0530

    Display stock price in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,5 +3,10 @@ class Stock < ApplicationRecord
     client = IEX::Api::Client.new(publishable_token:'Tpk_40fb2a3da0214f01912501a6bd6ee8c0',
                                     endpoint: 'https://sandbox.iexapis.com/v1')
       client.price(ticker_symbol)
+      begin
+     new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol))
+   rescue => exception
+     return nil
+   end
     end
 end
